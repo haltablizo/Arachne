0fdc7f57c223d3a7d3b93129a54dd401ab3f115b
Huawei <Huawei@LAPTOP-91CVFP3C>
2024-03-19 10:25:11 +0800

    Made basic scene changing work

--- src/controllers/InventoryController.java
+++ src/controllers/InventoryController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package controllers;
 
 import java.io.IOException;
@@ -15,20 +12,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Huawei
- */
-public class MainMenuController implements Initializable {
-
+public class InventoryController implements Initializable {
+   
     
-    @FXML Button exitButton, newGameButton; 
+    @FXML Button closeInvButton;
     
     @FXML 
-    private void newGame (ActionEvent event) throws IOException {
+    private void closeInv(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/Overworld.fxml"));
         Parent root = loader.load(); 
         OverworldController controller = loader.getController();
@@ -39,12 +32,7 @@ public class MainMenuController implements Initializable {
         thisStage.setScene(subjectScene);
         thisStage.show();
     }
-    @FXML
-    private void exitGame (ActionEvent event) throws IOException {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); 
-        stage.close(); 
-        
-    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/controllers/OverworldController.java
+++ src/controllers/OverworldController.java
@@ -1,16 +1,38 @@
 
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 
 public class OverworldController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
+    @FXML ImageView inventoryIcon;
+    
+    @FXML
+    private void openInv(Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/Inventory.fxml"));
+        Parent root = loader.load(); 
+        InventoryController controller = loader.getController();
+        
+        Scene subjectScene = new Scene(root);
+        Stage thisStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        thisStage.hide();
+        thisStage.setScene(subjectScene);
+        thisStage.show();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
