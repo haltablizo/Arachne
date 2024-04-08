7a0fb2318167af6f11155b1575cc58542174e567
Huawei <Huawei@LAPTOP-91CVFP3C>
2024-04-08 22:25:35 +0800

    currently working on inventory enumeration, not working

--- src/controllers/InventoryController.java
+++ src/controllers/InventoryController.java
@@ -1,7 +1,10 @@
 
 package controllers;
 
+import arachne.Inventory;
+import arachne.Powerup;
 import arachne.Spider;
+import arachne.Storeable;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +17,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class InventoryController implements Initializable {
@@ -23,6 +28,9 @@ public class InventoryController implements Initializable {
     @FXML Button useButton, dropButton;
     @FXML ImageView itemImage; 
     @FXML Label itemDesc, itemName; 
+    
+    @FXML GridPane potionGrid; 
+    
     private String openedFrom; 
     
     public void setOpenedFrom(String s) {
@@ -80,6 +88,40 @@ public class InventoryController implements Initializable {
 
         useButton.setVisible(false);
         dropButton.setVisible(false);
-    }    
+        
+        
+        
+//        int lastBox = 7%3 ;
+//        int lastRow = 7/3 ; 
+//        
+//        for (int i=0; i<lastRow+1; i++) {
+//            int c = 3; 
+//            if (i==lastRow) {
+//                c = lastBox; 
+//            }
+//            
+//            for (int j=0; j<c; j++) {
+//                Image img = new Image("/images/spiderIcon.png", 150, 150, false, false); 
+//                ImageView im = new ImageView(img);
+//                potionGrid.add(im,j,i);
+//            }
+//        }
+        
+        int tempI = Inventory.invPowerup.size()/3; 
+        int tempJ = Inventory.invPowerup.size()%3; 
+                
+        for (int i=0; i<tempI; i++) {
+            for(int j=0; j<3; j++) {
+                Image img = new Image("/images/spiderIcon.png", 150, 150, false, false); 
+                ImageView im = new ImageView(img);
+                potionGrid.add(im,j,i);
+            }
+        }
+        for (int i=0; i<tempJ; i++) {
+            Image img = new Image("/images/spiderIcon.png", 150, 150, false, false); 
+            ImageView im = new ImageView(img);
+            potionGrid.add(im, i, tempI);
+        }
+    }
     
 }
